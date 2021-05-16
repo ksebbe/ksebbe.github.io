596c2a60989ffe1cc22a4a328c5730ba08747c59
ksebbe <75369220+ksebbe@users.noreply.github.com>
2021-05-16 14:41:33 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,42 @@
+$(window).scroll(function() {
+  var scroll = $(window).scrollTop();
+	$(".zoom-me img").css({
+		width: (100 + scroll/5)  + "%",
+		top: -(scroll/10)  + "%",
+		"-webkit-filter": "blur(" + (scroll/200) + "px)",
+		filter: "blur(" + (scroll/200) + "px)"
+	});
+});let slideIndex = 1;
+showSlides(slideIndex);
+
+function nextSlide() {
+    showSlides(slideIndex += 1);
+}
+
+function previousSlide() {
+    showSlides(slideIndex -= 1);  
+}
+
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+
+    let slides = document.getElementsByClassName("item");
+    
+   
+    if (n > slides.length) {
+      slideIndex = 1
+    }
+    if (n < 1) {
+        slideIndex = slides.length
+    }
+  
+   
+    for (let slide of slides) {
+        slide.style.display = "none";
+    }
+    
+    slides[slideIndex - 1].style.display = "block";    
+}
